6365192569926f3d6a28accc831d89fae4db68fd
Tania Bezerra <nybrasil@gmail.com>
2024-05-18 14:40:29 -0400

    Finished apis

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,27 @@
+// Import the Sequelize library, which is an ORM (Object-Relational Mapper) for Node.js
 const Sequelize = require('sequelize');
+// Load environment variables from a .env file into process.env
 require('dotenv').config();
 
 let sequelize;
 
+// Check if the environment variable DB_URL is set (e.g., for a production environment)
 if (process.env.DB_URL) {
+  // If so, use the provided URL to connect to the database
   sequelize = new Sequelize(process.env.DB_URL);
 } else {
+  // If DB_URL is not set, fall back to using individual database credentials from environment variables
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_NAME,     // Database name
+    process.env.DB_USER,     // Database user
+    process.env.DB_PASSWORD, // Database user
     {
-      host: 'localhost',
-      dialect: 'postgres'
+      host: 'localhost',     // Hostname of the database server
+      dialect: 'postgres'    
     }
   );
 }
+// Export the configured Sequelize instance for use in other parts of the application
 module.exports = sequelize;
-//const db = {};
-//db.Sequelize = Sequelize;
-//db.sequelize = sequelize;
-//db.users = require('./user.js')(sequelize, Sequelize);
 
 
