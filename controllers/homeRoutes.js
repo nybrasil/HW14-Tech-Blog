f303e2e386eba980d1a2c0fd076de86b0d5335ac
Tania Bezerra <nybrasil@gmail.com>
2024-06-05 11:12:22 -0400

    controllers-homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
 
     const posts = postData.map((post) => post.get({ plain: true }));
 
-    res.render('home', { posts, loggedIn: req.session.logged_in });
+    res.render('homepage', { posts, loggedIn: req.session.logged_in });
   } catch (err) {
     res.status(500).json(err);
   }
